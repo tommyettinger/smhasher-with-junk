a7d89105c9b22e6dd5518426ac6b66bd117c35f5
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-12-20 22:29:13 -0800

    Try to incorporate more states....
    Ugh.

--- smhasher3/hashes/ax.cpp
+++ smhasher3/hashes/ax.cpp
@@ -769,6 +769,34 @@ static void ax(const void* in, const size_t len, const seed_t seed, void* out) {
 //    ----------------------------------------------------------------------------------------------
 //    Verification value is 0x00000001 - Testing took 177.348465 seconds
 
+// Using two of the abcd variables per qrst variable, it isn't much different.
+
+//----------------------------------------------------------------------------------------------
+//- log2(p - value) summary:
+//
+//0     1     2     3     4     5     6     7     8     9    10    11    12
+//---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- -
+//1697   436   219   100    55    29    19     5    12    16     5     2     3
+//
+//13    14    15    16    17    18    19    20    21    22    23    24    25 +
+//---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- - ---- -
+//1     1     3     0     1     0     0     2     2     2     0     1   164
+//
+//----------------------------------------------------------------------------------------------
+//Summary for: ax32
+//Overall result : FAIL(154 / 186 passed)
+//Failures :
+//    Sanity : [Basic 2]
+//    Avalanche : [64, 128]
+//    Sparse : [3 / 32, 3 / 48, 3 / 64, 3 / 96, 2 / 128, 2 / 256, 2 / 512, 2 / 1024, 2 / 1280]
+//    Permutation : [4 - bytes[3 high + low bits; LE], 4 - bytes[3 high + low bits; BE], 4 - bytes[0, low bit; LE], 4 - bytes[0, low bit; BE], 4 - bytes[0, high bit; LE], 4 - bytes[0, high bit; BE], 8 - bytes[0, low bit; LE], 8 - bytes[0, low bit; BE], 8 - bytes[0, high bit; LE], 8 - bytes[0, high bit; BE]]
+//    Text : [Long alnum first 1968 - 2128, Long alnum last 1968 - 2128, Long alnum first 4016 - 4176, Long alnum last 4016 - 4176, Long alnum first 8112 - 8272, Long alnum last 8112 - 8272]
+//    TwoBytes : [1024, 2048, 4096]
+//    Seed : [52]
+//
+//    ----------------------------------------------------------------------------------------------
+//    Verification value is 0x00000001 - Testing took 202.703342 seconds
+
 static const uint32_t C32 = UINT32_C(0xB89A8925);
 
 // truncated 64-bit, low 32 bits
@@ -811,10 +839,10 @@ static inline uint32_t mix_stream_bulk32(uint32_t h, uint32_t a, uint32_t b, uin
     //     + (ROTL32(c, R2) - b ^ h) * S32;
     //     + (ROTL32(d, R2) - a ^ h) * T32;
 
-    h += *q -= (ROTL32(a, R2)) * Q32;
-    h += *r -= (ROTL32(b, R2)) * R32;
-    h += *s -= (ROTL32(c, R2)) * S32;
-    h += *t -= (ROTL32(d, R2)) * T32;
+    h += *q += (ROTL32(a, R2) - c) * Q32;
+    h += *r += (ROTL32(b, R2) - d) * R32;
+    h += *s += (ROTL32(c, R2) - b) * S32;
+    h += *t += (ROTL32(d, R2) - a) * T32;
     return h;
 }
 
