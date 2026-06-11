0996fd9d2d1aed3de42633f4f7553d4f9a3a4e03
tommy <tommy.ettinger@gmail.com>
2026-06-10 22:41:26 -0700

    Got it passing!

--- smhasher3/hashes/pairaat.cpp
+++ smhasher3/hashes/pairaat.cpp
@@ -34,28 +34,25 @@
 
           0     1     2     3     4     5     6     7     8     9    10    11    12
         ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-         1855   466   210    99    63    33    19     6     6     2     4     0     1
+         1853   452   227   133    58    18    14     8     6     3     1     0     2
 
          13    14    15    16    17    18    19    20    21    22    23    24    25+
         ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-            2     0     0     1     2     0     0     0     0     0     0     0     6
+            0     0     0     0     0     0     0     0     0     0     0     0     0
 
 ----------------------------------------------------------------------------------------------
 Summary for: PairAAT
-Overall result: FAIL            ( 183 / 186 passed)
-Failures:
-    Bitflip             : [3, 4]
-    Seed                : [2]
+Overall result: pass            ( 186 / 186 passed)
 
 ----------------------------------------------------------------------------------------------
-Verification value is 0x00000001 - Testing took 184.416908 seconds
+Verification value is 0x00000001 - Testing took 184.633249 seconds
  */
 
 template <bool bswap>
 static uint32_t PairAAT_impl( const uint8_t * str, size_t len, uint32_t seed ) {
     const uint8_t * const end = str + len - 1;
-    uint32_t h1 = seed ^ 0x3b5db7f9u;
-    uint32_t h2 = ROTL32(seed, 15);
+    uint32_t h1 = seed + 0x3b5db7f9u;
+    uint32_t h2 = seed ^ ROTL32(seed, 10) ^ ROTL32(seed, 23);
 
     for (; str < end; str+= 2) {
         h1 += GET_U16<bswap>(str, 0);
@@ -66,22 +63,22 @@ static uint32_t PairAAT_impl( const uint8_t * str, size_t len, uint32_t seed ) {
     }
 
     if ((len & 1) == 1) {
-        h1 += end[0] + 0x3b5db7f9u;
+        h1 += end[0] + 0x9e3779b9u;
         h1 += h1 << 3; // h1 *= 9
         h2 += h1;
         h2  = ROTL32(h2, 7);
         h2 += h2 << 2; // h2 *= 5
     }
 
-    h1 ^= h2;
+    h1 ^= h2 ^ h2 >> 13;
     /*
      * now h1 passes all collision checks,
      * so it is suitable for hash-tables with prime numbers.
      */
     h1 += ROTL32(h2, 14);
-    h2 ^= h1; h2 += ROTR32(h1, 6);
+    h2 ^= h1; h2 += ROTL32(h1, 26);
     h1 ^= h2; h1 += ROTL32(h2, 5);
-    h2 ^= h1; h2 += ROTR32(h1, 8);
+    h2 ^= h1; h2 += ROTL32(h1, 24);
 
     return h2;
 }
@@ -109,8 +106,8 @@ REGISTER_HASH(PairAAT,
          FLAG_IMPL_LICENSE_MIT  |
          FLAG_IMPL_VERY_SLOW,
    $.bits = 32,
-   $.verification_LE = 0,
-   $.verification_BE = 0,
+   $.verification_LE = 0xD3BF4AF7,
+   $.verification_BE = 0xF884A602,
    $.hashfn_native   = PairAAT<false>,
    $.hashfn_bswap    = PairAAT<true>
  );
