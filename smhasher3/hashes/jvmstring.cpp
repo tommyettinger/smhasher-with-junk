bcfbe66fc7629cc0d983b7c648c2bbf40046c09d
tommy <tommy.ettinger@gmail.com>
2026-07-19 20:37:49 -0700

    Passes BIC, but fails some seed tests?

--- smhasher3/hashes/jvmstring.cpp
+++ smhasher3/hashes/jvmstring.cpp
@@ -1228,6 +1228,30 @@ Failures:
 ----------------------------------------------------------------------------------------------
 Verification value is 0x00000001 - Testing took 279.743789 seconds
 
+Trying to rearrange... worse! And slightly slower.
+----------------------------------------------------------------------------------------------
+-log2(p-value) summary:
+
+          0     1     2     3     4     5     6     7     8     9    10    11    12
+        ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+         4220  1188   560   269   162    79    40    14    12     8     0     2     0
+
+         13    14    15    16    17    18    19    20    21    22    23    24    25+
+        ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+            1     1     2     3     0     2     1     0     0     2     1     2   300
+
+----------------------------------------------------------------------------------------------
+Summary for: jvmstring6
+Overall result: FAIL            ( 145 / 187 passed)
+Failures:
+    Sparse              : [4/14, 4/16, 3/32, 3/48, 3/64, 3/96, 2/128, 2/256, 2/512, 2/1024, 2/1280]
+    TwoBytes            : [20, 32]
+    SeedBlockLen        : [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    SeedBlockOffset     : [0, 1, 2, 3, 4]
+
+----------------------------------------------------------------------------------------------
+Verification value is 0x00000001 - Testing took 294.198065 seconds
+
  */
 template <bool bswap>
 static void jvmstring6( const void * in, const size_t len, const seed_t seed, void * out ) {
@@ -1238,11 +1262,11 @@ static void jvmstring6( const void * in, const size_t len, const seed_t seed, vo
     h ^= ROTL64(h, 11) ^ ROTL64(h, 51);
 
     for (; i < len; data += 8, i += 8) {
-        h = (h ^ h >> 29) * 5555555555555555555UL + i;
         h += GET_U64<bswap>(data, 0);
+        h = (h ^ h >> 29) * 5555555555555555555UL + i;
     }
-    h = (h ^ h >> 29) * 5555555555555555555UL + len;
     switch (len & 7) {
+        case 0: goto NO_EXTRA;
         case 1: h += (data[0]); break;
         case 2: h += (GET_U16<bswap>(data, 0)); break;
         case 3: h += (GET_U16<bswap>(data, 0) + ((uint64_t)data[2] << 16)); break;
@@ -1252,6 +1276,8 @@ static void jvmstring6( const void * in, const size_t len, const seed_t seed, vo
         case 7: h += (GET_U32<bswap>(data, 0) + ((uint64_t)GET_U16<bswap>(data, 4) << 32) + ((uint64_t)data[6] << 48)); break;
     }
 
+    h = (h ^ h >> 29) * 5555555555555555555UL + len;
+NO_EXTRA:
     h ^= h >> 31;
     h += h * h | 0x65535UL;
     h ^= h >> 29 ^ len;
